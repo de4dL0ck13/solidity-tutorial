f44d2e47ee72fb9eebd9ed80a831519b923ba4a8
de4dL0ck13 <32480743+de4dL0ck13@users.noreply.github.com>
2017-12-14 15:22:15 +1100

    Update bank.sol

--- bank.sol
+++ bank.sol
@@ -1,25 +1,31 @@
-pragma solidity ^0.4.11;
+pragma solidity ^0.4.18;
+
+/// @title EtherBank
+/// @author De4dL0ck13
 
 contract EtherBank {
+    // owner will be able to destroy contract
     address public owner;
     
+    //dictionary mapping addresses to balances
     mapping (address => uint) private balances;
     
+    //event confirming deposit
     event DepositComplete(address account, uint amount);
     
-    function CoolBank() public {
+    function EtherBank() public {
         owner = msg.sender;
     }
     
     function deposit(address account) public payable returns (uint) {
         balances[account] += msg.value;
         
-        DepositComplete(account, msg.value);
+        DepositComplete(account, msg.value); //fire event
         
         return balances[account];
     }
     
-    function withdraw(uint amount) public returns (uint) {
+   function withdraw(uint amount) public returns (uint) {
         require(balances[msg.sender] >= amount);
         
         balances[msg.sender] -= amount;
@@ -28,5 +34,15 @@ contract EtherBank {
         }
         
         return balances[msg.sender];
+    }
+
+    function balance(address customer) public constant returns (uint){
+      return balances[customer];
+    }
+
+    function remove() public {
+      if (msg.sender == owner) {
+        selfdestruct(owner);
+      }
     }
 }
